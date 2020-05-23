bec37d81433ec4223f2b923b82973778b6922b7e
adepusai123 <adepusai123@gmail.com>
2020-05-23 22:33:45 +0530

    baseUrl added

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseUrl = environment.baseUrl;
   public loggedIn = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient) { }
@@ -15,12 +17,13 @@ export class AuthService {
   }
 
   loginUser(body: any) {
-    return this.http.get('http://localhost:3100/user');
+    return this.http.get(`${this.baseUrl}user`);
   }
 
   updateUserLoginStatus(val) {
     this.loggedIn.next(val);
   }
+  
   logOut() {
     this.loggedIn.next(false);
   }
