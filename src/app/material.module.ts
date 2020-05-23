f9c78e09cfd61337a361dcc011d927eca4388757
adepusai123 <adepusai123@gmail.com>
2020-05-23 16:57:13 +0530

    material modules added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
+const modulesList = [
+  CommonModule,
+  MatSidenavModule,
+  MatListModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule
+];
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: modulesList,
+  exports: modulesList
 })
 export class MaterialModule { }
